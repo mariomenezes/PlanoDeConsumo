2cc131005c60ca845c50ebe722eafdea9676f387
mariomenezes <mariomenezescosta@gmail.com>
2016-08-25 22:30:10 -0300

    primeiro commit

---
+++ src/energySaver/EquipmentManager.java
@@ -0,0 +1,25 @@
+package energySaver;
+
+import java.util.ArrayList;
+
+/*Just Store all equipments*/
+public class EquipmentManager {
+
+    private static ArrayList equipments = new ArrayList<Equipment>();
+
+    // Adiciona uma cidade destino
+    public static void addEquipment(Equipment equipment) {
+        equipments.add(equipment);
+    }
+    
+    // return an equipment
+    public static Equipment getEquipment(int index){
+        return (Equipment)equipments.get(index);
+    }
+    
+    // retorna a quantidade de cidades destino
+    // return the number of equipments
+    public static int numberOfEquipments(){
+        return equipments.size();
+    }
+}
